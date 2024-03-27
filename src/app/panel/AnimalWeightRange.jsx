bd265253299742172191c96ee5d1bf20214f0e88
nil729student <nil.pinana@cirvianum.cat>
2024-03-27 17:59:34 +0100

    rang inputs

--- src/app/panel/AnimalWeightRange.jsx
+++ src/app/panel/AnimalWeightRange.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 export default function AnimalWeightRange() {
     return(
-        <div className="">
-            <h1>Animal Weight Range</h1>
-            
-        </div>
+        <>
+            <dib>
+                <h1>Animal Weight Range</h1>
+                <input className="border" type="number" /> - <input className="border" type="number" />
+                <label htmlFor="class">Classification</label>
+                <input id="class" className="border" type="text" />
+                <label htmlFor="class">unitat</label>
+                <input className="border" type="number" />
+            </dib>
+        </>
     )
 }
